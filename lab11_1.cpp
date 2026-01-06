785bc7495bb85164ea0fa2c3480534553936f05f
pinkkykuki <panpink1509@gmail.com>
2026-01-07 00:00:14 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+
+    const char* grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "You will get " << grades[rand() % 9] << " in this 261102." << endl;
+
+    return 0;
+}
